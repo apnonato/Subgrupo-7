08eb7154eaf2b9a6dda4e9c2b056e08c9cb0c183
Thiago Sousa <thiagosousa81@gmail.com>
2024-12-17 14:17:13 -0300

    conv-temp

--- principal.c
+++ principal.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 
+// Função 4 - Conversor de temperatura - Thiago Sousa (github.com/thiagosousa81)
+void converterTemperatura(int opcao, float valor) {
+    float resultado;
+
+    switch (opcao) {
+        case 1: // Celsius para Fahrenheit
+            resultado = (valor * 9/5) + 32; // Fórmula de conversão de Celsius para Fahrenheit
+            printf("%.2f Celsius = %.2f Fahrenheit\n", valor, resultado);
+            break;
+
+        case 2: // Fahrenheit para Kelvin
+            resultado = (valor - 32) * 5/9 + 273.15; // Fórmula de conversão de Fahrenheit para Kelvin
+            printf("%.2f Fahrenheit = %.2f Kelvin\n", valor, resultado);
+            break;
+
+        case 3: // Kelvin para Celsius
+            resultado = valor - 273.15; // Fórmula de conversão de Kelvin para Celsius
+            printf("%.2f Kelvin = %.2f Celsius\n", valor, resultado);
+            break;
+
+        default:
+            printf("Opção inválida!\n");
+    }
+}
+
+// interface - Thiago Sousa (github.com/thiagosousa81)
 int main() {
-    int op = 0;
+    
+    int op;
     printf("Escolha uma das opções de conversor:\n");
     printf("1 - Comprimento\n2 - Massa\n3 - Volume\n4 - Temperatura\n5 - Velocidade\n6 - Potência\n7 - Área\n8 - Tempo\n9 - Armazenamento\n10 - Sair\n");
     scanf("%d", &op);
@@ -18,7 +45,14 @@ int main() {
             printf("Conversor de unidades de volume\n");
             break;
         case 4:
-            printf("Conversor de unidades de temperatura\n");
+            // Use esse exemplo para desenvolver sua parte
+            printf("Conversor de unidades de temperatura\nEscolha uma função\n1 - Celsius para Fahrenheit\n2 - Fahrenheit para Kelvin\n3 - Kelvin para Celsius\n");
+            scanf("%d", &op);
+            float valor;
+            printf("Digite um valor:");
+            scanf("%f", &valor);
+            converterTemperatura(op, valor);
+            
             break;
         case 5:
             printf("Conversor de unidades de velocidade\n");
